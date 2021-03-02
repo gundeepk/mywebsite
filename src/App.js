74d35d6772cdb536cea4ae16e3e50501091ac953
gundeepk <gundeepk123@gmail.com>
2021-03-02 15:14:37 -0500

    messing around

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
-
+import profileImage from './img/william-nylander.jpg'
 function App() {
   return (
     <div className="App">
-      <h1>Test Website</h1>
-      <h2>Author: Gundeep Kanwal</h2>
-    </div>
+      <h1>Nylander is the best player in the world, I love William Nylander</h1>
+      <h2>Author: Sanit Sharma</h2>
+      <img src={profileImage} alt="profile-image" />
+      </div>
   );
 }
 
